ef8afc76b531d0c4fcc52bf8af6ce6d86f774df3
Ignacio Buioli <ibuioli@gmail.com>
2020-05-22 01:04:00 -0300

    feat: check pricelist

--- models/models.py
+++ models/models.py
@@ -3,13 +3,41 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-# class ResCompany(models.Model):
-#     _inherit = 'res.company'
+import requests
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    sucursalOrigen = fields.Char(string='Sucursal de Origen', help='Sucursal desde donde se sale el pedido. Opcional')
 
 class DeliveryCarrier(models.Model):
     _inherit = 'delivery.carrier'
 
+    urlRateShipment = 'https://api.qa.andreani.com/v1/tarifas'
+
     delivery_type = fields.Selection(selection_add=[('andreani', 'Andreani')])
 
     def andreani_rate_shipment(self, order):
-        raise ValidationError('Prueba')
+        cpDestino = 0
+        sucursalOrigen = ''
+
+        if order.partner_shipping_id.zip == False:
+            raise ValidationError('¡El Cliente debe tener código postal!')
+        else:
+            cpDestino = order.partner_shipping_id.zip
+
+        if order.company_id.sucursalOrigen:
+            sucursalOrigen = order.company_id.sucursalOrigen
+
+        r = requests.get(url = self.urlRateShipment +
+        '?cpDestino=' + cpDestino + 
+        '&contrato=300006611' +
+        '&cliente=CL0003750' +
+        '&sucursalOrigen=' + sucursalOrigen +
+        '&bultos[0][valorDeclarado]=1200&bultos[0][volumen]=200&bultos[0][kilos]=1.3')
+        
+        if r.status_code == 200:
+            data = r.json()
+            raise ValidationError(data['pesoAforado'])
+        else:
+            raise ValidationError('Algo salió mal. Contacte con su Administrador.')
